3ffafba1e3d5b4b8aa1fd3d5dcb3f412a5e91917
JoseMiracle <josephmiracle119@gmail.com>
2022-11-03 01:10:26 -1100

    another commit

--- reverse_sentence.c
+++ reverse_sentence.c
@@ -3,14 +3,22 @@
 
 //Program to reverse sentence
 int main( ){
-	char sentence[1000];
+	int i = 0;
+	char entered_sentence[1000];
 	
 	printf("Enter the sentence: ");
-	fgets(sentence,1000,stdin);
+	fgets(entered_sentence,1000,stdin);
 	
-	char reversed_sentence[1000];
-	strcpy(reversed_sentence, strrev(sentence));	
-	printf("%s", reversed_sentence);	
+	int length_of_sentence = strlen(entered_sentence);
+	int length_of_sentence_in_array = length_of_sentence - 1;
 	
+	printf("***The Entered Sentence ***\n");
+	printf("%s \n", entered_sentence);
+	printf("***Reversed Sentence***");
+	
+	for(int number = length_of_sentence_in_array; number >= i ; number--){
+		printf("%c", entered_sentence[number]); 		
+	}	
+
 	return 0;
 }
